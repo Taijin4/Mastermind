9c211ba349453c4d630d38c409eabff3d20c9edc
Leo Kiefner <147720183+LeoKiefner@users.noreply.github.com>
2023-12-18 09:14:49 +0100

    Update jusqu'au System.exit

--- mastermind/src/View/StartScreen.java
+++ mastermind/src/View/StartScreen.java
@@ -2,7 +2,6 @@ package View;
 import javax.swing.*;
 import java.awt.*;
 
-
 public class StartScreen extends JFrame {
 
     public StartScreen() {
@@ -10,20 +9,37 @@ public class StartScreen extends JFrame {
         setSize(400, 500);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        JButton button1 = new JButton("Lancer une partie");
-        JButton button3 = new JButton("Quitter");
-
         JPanel panel = new JPanel(new GridBagLayout());
+        panel.setBackground(Color.ORANGE); // Définir le fond en jaune
+
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridwidth = GridBagConstraints.REMAINDER;
         gbc.anchor = GridBagConstraints.CENTER;
         gbc.insets = new Insets(5, 0, 5, 0); // Des marges pour l'espacement
 
-        // Ajouter les boutons au panel avec les contraintes
-        panel.add(button1, gbc);
+        // Charger et redimensionner l'image
+        ImageIcon originalIcon = new ImageIcon("mastermind/src/View/logo.png");
+        Image image = originalIcon.getImage();
+        Image newimg = image.getScaledInstance(400, 150, Image.SCALE_DEFAULT);
+        ImageIcon icon = new ImageIcon(newimg);
+
+        JButton button2 = new JButton("Commencer");
+        JButton button3 = new JButton("Quitter");
+
+        // Ajouter les composants au panel
+        panel.add(new JLabel(icon), gbc);
+        panel.add(button2, gbc);
         panel.add(button3, gbc);
+
+        button3.addActionListener( actionEvent  -> {
+            System.exit(0);
+        });
 
         setContentPane(panel);
         setVisible(true);
+
+
     }
+
+
 }
