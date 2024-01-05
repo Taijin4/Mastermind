3a2bfa2f1e69088212992d347b7d77d70f74d7b8
Taijin4 <antoningoisque3@gmail.com>
2024-01-05 20:05:18 +0100

    Ajout écran de fin

---
+++ mastermind/src/View/EndScreen.java
@@ -0,0 +1,40 @@
+package View;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class EndScreen extends JFrame {
+
+	public EndScreen(int score) {
+		setTitle("CScore");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(new Dimension(810, 1000));
+		setVisible(true);
+		setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+		String text =  String.valueOf(score);
+
+		JPanel mainPanel = new JPanel(new BorderLayout());
+		JPanel titlePanel = new JPanel(new GridLayout(2, 1));
+		JPanel scorePanel = new JPanel(new BorderLayout());
+
+		JLabel title = new JLabel("Bravo, vous avez finis toutes les manches !", SwingConstants.CENTER);
+		title.setFont(new Font("Arial", Font.PLAIN, 36)); // Taille de police 36
+		titlePanel.add(title);
+
+		JLabel titleScore = new JLabel("Votre score est de : ", SwingConstants.CENTER);
+		titleScore.setFont(new Font("Arial", Font.PLAIN, 36)); // Taille de police 36
+		titlePanel.add(titleScore);
+
+		JLabel scoreLabel = new JLabel(text, SwingConstants.CENTER);
+		scoreLabel.setFont(new Font("Arial", Font.PLAIN, 36)); // Taille de police 36
+		scorePanel.add(scoreLabel);
+
+		mainPanel.add(titlePanel, BorderLayout.NORTH);
+		mainPanel.add(scorePanel, BorderLayout.CENTER);
+
+		setVisible(true);
+		setContentPane(mainPanel);
+	}
+
+}
