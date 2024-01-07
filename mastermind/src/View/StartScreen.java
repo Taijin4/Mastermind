349a55985dca8d30483db1fd8ae19c247784fc39
LeoKiefner <leokiefner.pro@gmail.com>
2024-01-07 13:57:09 +0100

    Ajout du slider du nombre de tentatives et du slider de difficulté

--- mastermind/src/View/StartScreen.java
+++ mastermind/src/View/StartScreen.java
@@ -1,6 +1,8 @@
 package View;
 import Controller.MastermindController;
 import Model.MastermindGame;
+import java.util.Dictionary;
+import java.util.Hashtable;
 
 import javax.swing.*;
 import java.awt.*;
@@ -9,7 +11,7 @@ public class StartScreen extends JFrame {
 
     public StartScreen() {
         super("Start");
-        setSize(400, 550);
+        setSize(400, 700);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         JPanel panel = new JPanel(new GridBagLayout());
         panel.setBackground(Color.ORANGE);
@@ -35,7 +37,7 @@ public class StartScreen extends JFrame {
         JSlider slideRounds = new JSlider();
         slideRounds.setBackground(Color.ORANGE);
         slideRounds.setMaximum(5);
-        slideRounds.setMinimum(0);
+        slideRounds.setMinimum(1);
         slideRounds.setValue(3);
         slideRounds.setPaintTicks(true);
         slideRounds.setPaintLabels(true);
@@ -47,7 +49,7 @@ public class StartScreen extends JFrame {
         JSlider slideColorsNumber = new JSlider();
         slideColorsNumber.setBackground(Color.ORANGE);
         slideColorsNumber.setMaximum(8);
-        slideColorsNumber.setMinimum(0);
+        slideColorsNumber.setMinimum(1);
         slideColorsNumber.setValue(8);
         slideColorsNumber.setPaintTicks(true);
         slideColorsNumber.setPaintLabels(true);
@@ -59,12 +61,41 @@ public class StartScreen extends JFrame {
         JSlider slideHoleNumber = new JSlider();
         slideHoleNumber.setBackground(Color.ORANGE);
         slideHoleNumber.setMaximum(6);
-        slideHoleNumber.setMinimum(0);
+        slideHoleNumber.setMinimum(1);
         slideHoleNumber.setValue(4);
         slideHoleNumber.setPaintTicks(true);
         slideHoleNumber.setPaintLabels(true);
         slideHoleNumber.setMinorTickSpacing(1);
         slideHoleNumber.setMajorTickSpacing(1);
+
+        JLabel labelAttempts = new JLabel("Nombre de tentatives");
+
+        JSlider slideAttempts = new JSlider();
+        slideAttempts.setBackground(Color.ORANGE);
+        slideAttempts.setMaximum(12);
+        slideAttempts.setMinimum(1);
+        slideAttempts.setValue(10);
+        slideAttempts.setPaintTicks(true);
+        slideAttempts.setPaintLabels(true);
+        slideAttempts.setMinorTickSpacing(1);
+        slideAttempts.setMajorTickSpacing(1);
+
+        JLabel labelDifficulty = new JLabel("Difficulté");
+
+        JSlider slider = new JSlider(JSlider.HORIZONTAL, 0, 2, 1);
+        slider.setBackground(Color.ORANGE);
+        slider.setPaintTicks(true);
+        slider.setPaintLabels(true);
+        slider.setMinorTickSpacing(1);
+        slider.setMajorTickSpacing(1);
+
+        // Personnalisation des labels
+        Dictionary<Integer, JLabel> labelTable = new Hashtable<>();
+        labelTable.put(0, new JLabel("Facile"));
+        labelTable.put(1, new JLabel("Classique"));
+        labelTable.put(2, new JLabel("Numérique"));
+        slider.setLabelTable(labelTable);
+
 
         JButton buttonBack = new JButton("Retour");
         JButton buttonPlay = new JButton("Jouer");
@@ -85,6 +116,10 @@ public class StartScreen extends JFrame {
             panel.add(slideColorsNumber, gbc);
             panel.add(labelHoleNumber, gbc);
             panel.add(slideHoleNumber, gbc);
+            panel.add(labelAttempts, gbc);
+            panel.add(slideAttempts, gbc);
+            panel.add(labelDifficulty, gbc);
+            panel.add(slider, gbc);
 
             gbc.gridwidth = 1;
             gbc.weightx = 0;
@@ -124,7 +159,7 @@ public class StartScreen extends JFrame {
 
         buttonPlay.addActionListener( actionEvent ->{
 //            System.out.println("Slide colors : " + slideColorsNumber.getValue() + ", Slide rounds : " + slideRounds.getValue() + ", Slide hole number : " + slideHoleNumber.getValue());
-            MastermindGame model = new MastermindGame(slideColorsNumber.getValue(), slideRounds.getValue(), slideHoleNumber.getValue());
+            MastermindGame model = new MastermindGame(slideColorsNumber.getValue(), slideAttempts.getValue(), slideHoleNumber.getValue());
             MastermindController controller = new MastermindController(model);
             controller.startGame();
             StartScreen.this.dispose();
