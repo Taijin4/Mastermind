c500f21f030ac388b02dc632ba889678de5ae1ad
LeoKiefner <leokiefner.pro@gmail.com>
2024-01-10 13:56:01 +0100

    Reglage des images

--- mastermind/src/Model/Combination.java
+++ mastermind/src/Model/Combination.java
@@ -6,7 +6,6 @@ import java.util.Collections;
 import java.util.Random;
 
 public class Combination {
-
     ArrayList<Color> colors;
     public Combination(int nbColors) {
         this.colors = new ArrayList<>(nbColors);
